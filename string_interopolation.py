94aad48377c34ba7a7daacc6a3d3795a0b6281f3
Stephen Roberts <Sroberts12321@gmail.com>
2018-05-01 12:10:22 -0500

    adding the inital files

---
+++ string_interopolation.py
@@ -0,0 +1,4 @@
+first_name = str(input("Please enter your first name: "))
+last_name = str(input("Please enter your last name: "))
+print (f"{first_name} {last_name} is your full name, not that you didn't know that")
+#print(f"You entered {first_name} right?")
